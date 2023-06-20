1891b13385a8a9ea1a1bd50f15f6b96a7ace4a79
devCKVargas <50130192+devCKVargas@users.noreply.github.com>
2023-06-20 18:00:28 +0800

    Day 9: Add: utilities & script.js

---
+++ Projects/Trillo/src/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//? HTML elements
+const userBookmark = document.querySelector("#user-bookmark");
+const userChat = document.querySelector("#user-chat");
+const userProfile = document.querySelector("#user-profile");
+
+//? Event listeners
+userBookmark.addEventListener("click", () => {});
+userChat.addEventListener("click", () => {});
+
+userProfile.addEventListener("click", () => {
+	//? Show profile page
+});

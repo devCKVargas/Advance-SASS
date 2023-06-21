da56379b496283f7d8be375c4c0195452a95c8f2
devCKVargas <50130192+devCKVargas@users.noreply.github.com>
2023-06-21 15:52:43 +0800

    Modified: comments for better readability

--- Projects/Natours/src/js/script.js
+++ Projects/Natours/src/js/script.js
@@ -1,31 +1,29 @@
 "use strict";
 
-//$ Select HTML elements
+//? Select HTML elements
 const popup = document.querySelector(".popup__content");
 const navContainer = document.querySelector(".navigation__list");
 const navMenu = document.getElementById("navi-toggle");
-const navLinks = document.querySelectorAll(".navigation__link"); //$ 0️⃣
+const navLinks = document.querySelectorAll(".navigation__link"); // 0️⃣
 
-//$ Event listener for clicking anywhere on the DOM
+//? Event listener for clicking anywhere on the DOM
 document.addEventListener("click", (event) => {
-	//$ if clicked element is not a child of popup__content && when URL includes #popup
+	// if clicked element is not a child of popup__content && when URL includes #popup
 	if (!popup.contains(event.target) && window.location.href.includes("#popup")) {
-		//$ close popup
-		//$ closing animation still plays
-		//$ since there is no #popup-closed in HTML
+		// close popup | closing animation still plays since there is no #popup-closed in HTML
 		window.location.href = "#closed";
 	}
 
-	//$ if clicked target is not a child of navMenu
+	// if clicked target is not a child of navMenu
 	if (!navMenu.contains(event.target)) {
 		navMenu.addEventListener("click", () => {
 			navMenu.checked = !navMenu.checked;
 		});
-		//$ extract links from navLinks array 0️⃣
+
+		// extract links from navLinks array 0️⃣
 		for (const link of navLinks) {
-			//$ if clicked target is not any link
+			// close navMenu if clicked target is not any link
 			if (!link.contains(event.target)) {
-				//$ close navMenu
 				navMenu.checked = false;
 			}
 		}
